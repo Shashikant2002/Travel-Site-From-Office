11d175555253094d4df62cf2b5225f0d30a0cdb6
Shashikant <shashikant384443>
2022-12-10 14:37:53 +0530

    new header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,13 @@ import { FiPhoneCall } from 'react-icons/fi';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { CgMenuRight } from 'react-icons/cg';
 import { AiFillCloseCircle } from 'react-icons/ai';
+import profile from '../../Assets/Image/postThumb.jpg';
 // import { Link } from 'react-router-dom';
 import Logo from '../../Assets/Image/logo.png';
 import Socal from '../Socal/Socal';
 import Nav from '../Nav/Nav';
+import { FiMail } from 'react-icons/fi';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
 
 const Header = () => {
     const openContactMobile = () => {
@@ -18,26 +21,33 @@ const Header = () => {
         const contactHeader = document.getElementById('contactHeader');
         contactHeader.classList.remove('contentActive');
     }
+
     const sidemenuOpen = () => {
         const sideMenu = document.getElementById('sideMenu');
-        sideMenu.style.display = 'block';
+        sideMenu.classList.add('showSideMenu');
         sideMenu.children[0].style.right = '0%';
-        sideMenu.children[0].style.animation = 'sidemenuInAnim .3s linear'
     }
     const sidemenuClose = () => {
         const sideMenu = document.getElementById('sideMenu');
-        sideMenu.children[0].style.animation = 'sidemenuInAnim .3s linear'
-        sideMenu.style.display = 'none';
+        sideMenu.classList.remove('showSideMenu');
         sideMenu.children[0].style.right = '-100%';
     }
-
+    
+    const openSearchBox = () => {
+        const searchBox = document.getElementById('searchBox');
+        searchBox.classList.add('searchBoxActive');
+    }
+    const closeSearchBox = () => {
+        const searchBox = document.getElementById('searchBox');
+        searchBox.classList.remove('searchBoxActive');
+    }
     return (
         <>
             <header className='header'>
                 <div className="container">
                     <div className="topHead flex justifyContentBetween alignItemCenter">
                         <div className="contact flex alignItemCenter">
-                            <div className="icon colorWhite" onClick={() => openContactMobile()}>
+                            <div className="icon colorWhite flex" onClick={() => openContactMobile()}>
                                 <FiPhoneCall />
                             </div>
                             <div id='contactHeader' className="content">
@@ -51,14 +61,25 @@ const Header = () => {
                         </figure>
                         <div className="menuSearch flex alignItemCenter">
                             <div className="search colorWhite flex justifyContentCenter alignItemCenter">
-                                <BiSearchAlt2 />
+                                <span onClick={() => openSearchBox()} className='flex justifyContentCenter alignItemCenter'><BiSearchAlt2 /></span>
+                                <div className="searchBox flex justifyContentCenter alignItemCenter" id='searchBox'>
+                                    <div onClick={() => closeSearchBox()} className="close">
+                                        <AiFillCloseCircle />
+                                    </div>
+                                    <form action="/" className='flex flexDirCol'>
+                                        <input type="text" placeholder='Search Heere' />
+                                        <button className='globalBtn' type='sumbit'>Search</button>
+                                    </form>
+                                </div>
                             </div>
                             <div className="menu colorWhite" onClick={() => sidemenuOpen()}>
                                 <CgMenuRight />
                             </div>
                         </div>
                     </div>
-                    <div className="mainHeader flex justifyContentBetween alignItemCenter">
+                </div>
+                <div className="mainHeader">
+                    <div className="container flex justifyContentBetween alignItemCenter">
                         <Socal />
                         <Nav />
                         <a className='globalBtn' href="/">BOOK NOW</a>
@@ -66,8 +87,25 @@ const Header = () => {
                 </div>
 
                 <div className="sideMenu" id='sideMenu'>
-                    <div className="menu">
+                    <div className="menu flex flexDirCol justifyContentCenter alignItemCenter">
                         <span onClick={() => sidemenuClose()} className="close"><AiFillCloseCircle /></span>
+                        <div className="asideContent flex flexDirCol justifyContentCenter alignItemCenter">
+                            <h5>OUR PROPRIETOR</h5>
+                            <figure className="profile">
+                                <img src={profile} alt="ProfileImage" />
+                            </figure>
+                            <h4>James Watson</h4>
+                            <p className='textAlignCenter'>Accumsan? Aliquet nobis doloremque, aliqua? Inceptos voluptatem, duis tempore optio quae animi viverra distinctio cumque vivamus, earum congue, anim velit</p>
+                            <Socal />
+                            <h5>CONTACT US</h5>
+
+                            <div className="content textAlignCenter">
+                                <p>Feel free to contact and reach us !!</p>
+                                <p className='flex alignItemCenter justifyContentCenter'><FiPhoneCall /> +01(988) 256 203</p>
+                                <p className='flex alignItemCenter justifyContentCenter'><FiMail /> info@domain.com</p>
+                                <p className='flex alignItemCenter justifyContentCenter'><HiOutlineLocationMarker /> 3146 Koontz, California</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </header>
