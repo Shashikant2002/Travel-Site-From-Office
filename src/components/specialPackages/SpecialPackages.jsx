8fff33e94f8e32b21503669108598eac3dfa9ac0
Shashikant <shashikantprajapti17@gmail.com>
2022-12-11 18:46:29 +0530

    start new modules

---
+++ src/components/specialPackages/SpecialPackages.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import "./special.css";
+import { MdWatchLater } from "react-icons/md";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { ImLocation } from "react-icons/im";
+import packageImg from "../../Assets/Image/specialoffer.jpg";
+
+const SpecialPackages = () => {
+  const sectionHeading = {
+    topText: "OFFER & DISCOUNT",
+    heading: "OUR SPECIAL PACKAGES",
+    description:
+      "Fusce hic augue velit wisi quibusdam pariatur, iusto primis, nec nemo, rutrum. Vestibulum cumque laudantium. Sit ornare mollitia tenetur, aptent.",
+  };
+  return (
+    <>
+      <div className="commonSection specialPackages">
+        <div className="container textAlignCenter">
+          <SectionHeading options={sectionHeading} />
+          <div className="content flex justifyContentBetween">
+            <div className="card" style={{ background: `url(${packageImg})` }}>
+              <div className="textContent">
+                <div className="detail flex">
+                  <p className="flex alignItemCenter">
+                    <MdWatchLater /> 7D/6N
+                  </p>
+                  <p>|</p>
+                  <p className="flex alignItemCenter">
+                    <BsFillPeopleFill /> pax: 10
+                  </p>
+                  <p>|</p>
+                  <p className="flex alignItemCenter">
+                    <ImLocation /> Malaysia
+                  </p>
+                </div>
+                <h4>TOUR TO SATORINI</h4>
+                <p>
+                  Fusce hic augue velit wisi ips quibus dam pariatur, iusto.
+                </p>
+                <h4 className="colorDark">price: $1300 $1105</h4>
+                <a className="globalBtn" href="/">
+                  BOOK NOW
+                </a>
+              </div>
+            </div>
+            <div className="card" style={{ background: `url(${packageImg})` }}>
+              <div className="textContent">
+                <div className="detail flex">
+                  <p className="flex alignItemCenter">
+                    <MdWatchLater /> 7D/6N
+                  </p>
+                  <p>|</p>
+                  <p className="flex alignItemCenter">
+                    <BsFillPeopleFill /> pax: 10
+                  </p>
+                  <p>|</p>
+                  <p className="flex alignItemCenter">
+                    <ImLocation /> Malaysia
+                  </p>
+                </div>
+                <h4>TOUR TO SATORINI</h4>
+                <p>
+                  Fusce hic augue velit wisi ips quibus dam pariatur, iusto.
+                </p>
+                <h4 className="colorDark">price: $1300 $1105</h4>
+                <a className="globalBtn" href="/">
+                  BOOK NOW
+                </a>
+              </div>
+            </div>
+          </div>
+          <a className="globalBtn" href="/">
+            VIEW ALL PACKAGES
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SpecialPackages;
---
+++ src/components/videoContent/VideoContent.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./video.css";
+import { BsFillPlayFill } from "react-icons/bs";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+const VideoContent = () => {
+  const showPopup = () => {
+    const popup = document.getElementById("popup");
+    popup.classList.add("popupActive");
+  };
+  const closePopup = () => {
+    const popup = document.getElementById("popup");
+    const video = document.getElementById("mainYoutubeVideo");
+    popup.classList.remove("popupActive");
+    video.contentWindow.postMessage(
+      '{"event":"command","func":"' + "pauseVideo" + '","args":""}',
+      "*"
+    );
+  };
+  return (
+    <>
+      <div className="commonSection videoContent">
+        <div className="container">
+          <div className="videoIcon" onClick={() => showPopup()}>
+            <div className="colorWhite flex playIcon">
+              <BsFillPlayFill />
+            </div>
+          </div>
+
+          <div className="content textAlignCenter">
+            <h3 className="colorWhite">
+              ARE YOU READY TO TRAVEL? REMEMBER US !!
+            </h3>
+            <p className="colorWhite">
+              Fusce hic augue velit wisi quibusdam pariatur, iusto primis, nec
+              nemo, rutrum. Vestibulum cumque laudantium. Sit ornare mollitia
+              tenetur, aptent.
+            </p>
+            <div className="buttons flex justifyContentCenter">
+              <button className="globalBtn">LEARN MORE</button>
+              <button className="globalBorderBtn">BOOK NOW</button>
+            </div>
+          </div>
+
+          <div className="counter flex">
+            <div className="card">
+              <h3>80K+</h3>
+              <p>SATISFIED CUSTOMER</p>
+            </div>
+            <div className="card">
+              <h3>80K+</h3>
+              <p>SATISFIED CUSTOMER</p>
+            </div>
+            <div className="card">
+              <h3>80K+</h3>
+              <p>SATISFIED CUSTOMER</p>
+            </div>
+            <div className="card">
+              <h3>80K+</h3>
+              <p>SATISFIED CUSTOMER</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="popup flex justifyContentCenter alignItemCenter"
+          id="popup"
+        >
+          <iframe
+            id="mainYoutubeVideo"
+            className="popupVideo"
+            src="https://www.youtube.com/embed/PzGoCfFCgdw?enablejsapi=1&version=3&playerapiid=ytplayer"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <div className="close colorWhite" onClick={() => closePopup()}>
+            <AiFillCloseCircle />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VideoContent;
