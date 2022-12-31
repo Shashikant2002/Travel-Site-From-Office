1f66542d9798f584f2495276837a322b1e69a31c
Shashikant <shashikant384443>
2022-12-31 14:41:48 +0530

    Loading added

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SectionBanner from '../components/SectionBanner/SectionBanner';
 import bannerImg from '../Assets/Image/bannerBackground_1.jpg';
+import ContactCard from '../components/contactCard/ContactCard';
+
 
 const ContactUs = () => {
+
     return (
         <>
             <div className="destination">
                 <SectionBanner image={bannerImg} heading={"CONTACT US"} />
+                <ContactCard />
             </div>
         </>
     )
---
+++ src/components/contactCard/ContactCard.jsx
@@ -0,0 +1,16 @@
+import "./contactcard.css";
+import React from 'react';
+
+const ContactCard = () => {
+  return (
+    <>
+        <div className="contactCard commonSection">
+            <div className="container">
+                kjfklsdj
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default ContactCard
