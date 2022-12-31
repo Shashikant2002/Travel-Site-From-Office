bbc52e6cd3554dc0b973b6e854ae1416d8091cd6
Shashikant <shashikant384443>
2022-12-31 15:31:28 +0530

    .

--- src/components/contactCard/ContactCard.jsx
+++ src/components/contactCard/ContactCard.jsx
@@ -1,14 +1,41 @@
 import "./contactcard.css";
 import React from 'react';
+import { GoLocation } from 'react-icons/go';
 
 const ContactCard = () => {
   return (
     <>
-        <div className="contactCard commonSection">
-            <div className="container">
-                kjfklsdj
+      <div className="contactCard commonSection">
+        <div className="container flex justifyContentCenter flexWrap">
+          <div className="card flex alignItemCenter flexDirCol textAlignCenter">
+            <div className="icon">
+              <GoLocation />
             </div>
+            <h4>ADDRESS LOCATION</h4>
+            <p>3146 Koontz, California <br />
+              Quze.24 Second floor <br />
+              36 Street, Melbourne</p>
+          </div>
+          <div className="card flex alignItemCenter flexDirCol textAlignCenter">
+            <div className="icon">
+              <GoLocation />
+            </div>
+            <h4>ADDRESS LOCATION</h4>
+            <p>3146 Koontz, California <br />
+              Quze.24 Second floor <br />
+              36 Street, Melbourne</p>
+          </div>
+          <div className="card flex alignItemCenter flexDirCol textAlignCenter">
+            <div className="icon">
+              <GoLocation />
+            </div>
+            <h4>ADDRESS LOCATION</h4>
+            <p>3146 Koontz, California <br />
+              Quze.24 Second floor <br />
+              36 Street, Melbourne</p>
+          </div>
         </div>
+      </div>
     </>
   )
 }
